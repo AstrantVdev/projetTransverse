a1f72957687832df5a1e30dbb2f1c125368eb80a
AstrantV <AstrantV.creator@gmail.com>
2023-02-03 22:53:44 +0100

    render

--- entity.py
+++ entity.py
@@ -1,27 +1,42 @@
 import pygame
+from main import Game
+
+
+class Blocks(pygame.sprite.Sprite, Game):
+    def __init__(self, img, x=350, y=350):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.img = pygame.image.load("graphics/", img, ".png").convert_alpha()
+        self.rect = self.img.get_rect()
+
+    def blit(self):
+        self.screen.blit(self.img, self.rect)
+
 
 class Entity(pygame.sprite.Sprite):
 
-	def __init__(self):
-		super().__init__()
-		self.name = 'name'
-		self.weight = 50
-		self.life = -1
-		self.fly = False
-		self.x = 0
-		self.y = 0
-		self.pitch = 0
-		self.texture
-		self.type = "PROJECTILE"
-		self.mod = 0
+    def __init__(self):
+        super().__init__()
+        self.name = 'name'
+        self.weight = 50
+        self.life = -1
+        self.fly = False
+        self.x = 0
+        self.y = 0
+        self.pitch = 0
+        self.type = "PROJECTILE"
+        self.mod = 0
+
 
 class Player(Entity):
 
-	def __init__(self):
-		self.name="Player"
-		self.weight=150
-		self.life=500556448879
-		self.type="PLAYER"
-		self.nickname=""
-		self.inventory=[50]
-		self.texture = pygame.image.load("graphics/logo32x32.jpg")
+    def __init__(self):
+        super().__init__()
+        self.name = "Player"
+        self.weight = 20
+        self.life = 5
+        self.type = "PLAYER"
+        self.nickname = "nicknome"
+        self.inventory = [50]
+        self.texture = pygame.image.load("graphics/logo32x32.jpg")
--- main.py
+++ main.py
@@ -1,31 +1,57 @@
 import pygame
-import entity
+
 class Scene():
-     def __init__(self):
-         self.name = "name"
-         self.entities = []
-         self.userInterfaces = []
-         self.userInterfaceIndex = 0
-         self.blocks = []
-         self.fronts = []
+    def __init__(self):
+        self.name = "name"
+        self.userInterfaces = []
+        self.userInterfaceIndex = 0
+        self.center = [350, 350]
+        self.blocks = []
+        self.entities = []
+        self.fronts = []
+        self.backgrounds = []
 
-     def addEntity(self, entity):
-         self.entities.append(entity)
-         return self
+    def renderAll(self):
+        for b in self.blocks:
+            b.blit()
 
-     def addUserInterface(self, userInterface):
-         self.userInterfaces.append(userInterface)
-         return self
+        for e in self.entities:
+            e.blit()
 
-     def addBlock(self, block):
-         self.blocks.append(block)
-         return self
+        for f in self.fronts:
+            f.blit()
 
-     def addFront(self, front):
-         self.fronts.append(front)
-         return self
+        for b in self.backgrounds:
+            b.blit()
 
-class Game():
+    def renderElement(self, e):
+        c = self.center
+
+        if(c[0]-350 < e.x < c[0]+350):
+
+            if (c[1] - 350 < e.y < c[1] + 350):
+                x = c[0]-(350-e.x)
+                y = c[1]-(350-e.x)
+                e.blit(x, y)
+
+    def addEntity(self, entity):
+        self.entities.append(entity)
+        return self
+
+    def addUserInterface(self, userInterface):
+        self.userInterfaces.append(userInterface)
+        return self
+
+    def addBlock(self, block):
+        self.blocks.append(block)
+        return self
+
+    def addFront(self, front):
+        self.fronts.append(front)
+        return self
+
+
+class Game:
     def __init__(self):
         self.scene = ""
         self.running = False
@@ -64,27 +90,15 @@ class Game():
             if event.type == pygame.QUIT:
                 self.running = False
                 pygame.quit()
-                #ferme la fenetre
+                # ferme la fenetre
                 exit()
 
 
-class Blocks(pygame.sprite.Sprite, Game):
-    def __init__(self, img, x = 350, y = 350):
-        super().__init__()
-        self.x = x
-        self.y = y
-        self.img = pygame.image.load("graphics/",img,".png").convert_alpha()
-        self.rect = self.img.get_rect()
-
-    def blit(self):
-        self.screen.blit(self.img, self.rect)
-
-
 class Font(Game):
-    def __init__(self, text, x = 350, y = 350, color = (111, 196, 169), backGround = None):
+    def __init__(self, text, x=350, y=350, color=(111, 196, 169), backGround=None):
         self.x = x
         self.y = y
-        self.text= text
+        self.text = text
         self.color = color
         self.backGround = backGround
 
@@ -93,8 +107,10 @@ class Font(Game):
         game_message_rect = game_message.get_rect(center=(self.x, self.y))
         self.screen.blit(game_message, game_message_rect)
 
-def getFont(tall = 50):
+
+def getFont(tall=50):
     return pygame.font.Font('font/Pixeltype.ttf', tall)
+
 
 GAME = 'the game object'
 if __name__ == "__main__":
