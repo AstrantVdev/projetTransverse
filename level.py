68c1d1b990a49dc83ac4ddf5531a733b8fdfd99c
Djamel <123745346+Djamel2s@users.noreply.github.com>
2023-02-04 11:09:54 +0100

    world (video) pour pas le perdre

--- level.py
+++ level.py
@@ -1,30 +1,33 @@
 import pygame
+
 class World():
-	def __init__(self, level):
+	def __init__(self, data):
 		self.tile_list = []
+		self.screen = pygame.display.set_mode((700, 700))
+		#load images
 		dirt_img = pygame.image.load('img/dirt.png')
 		grass_img = pygame.image.load('img/grass.png')
-		ligne = 0
-		for ligne in level:
-			colonne = 0
-			for tile in ligne:
-                if tile == 1:
+		row_count = 0
+		for row in data:
+			col_count = 0
+			for tile in row:
+				if tile == 1:
 					img = pygame.transform.scale(dirt_img, (50, 50))
 					img_rect = img.get_rect()
-					img_rect.x = colonne * 50
-					img_rect.y = ligne * 50
+					img_rect.x = col_count * 50
+					img_rect.y = row_count * 50
 					tile = (img, img_rect)
 					self.tile_list.append(tile)
 				if tile == 2:
 					img = pygame.transform.scale(grass_img, (50, 50))
 					img_rect = img.get_rect()
-					img_rect.x = colonne * 50
-					img_rect.y = ligne * 50
+					img_rect.x = col_count * 50
+					img_rect.y = row_count * 50
 					tile = (img, img_rect)
 					self.tile_list.append(tile)
-				colonne += 1
-		 ligne += 1
+				col_count += 1
+			row_count += 1
 
 	def draw(self):
 		for tile in self.tile_list:
-			screen.blit(tile[0], tile[1])
+			self.screen.blit(tile[0], tile[1])
--- main.py
+++ main.py
@@ -1,133 +1,38 @@
 import pygame
-import entity
 import level
-class Scene():
-     def __init__(self):
-         self.name = "name"
-         self.entities = []
-         self.userInterfaces = []
-         self.userInterfaceIndex = 0
-         self.blocks = []
-         self.fronts = []
 
-     def addEntity(self, entity):
-         self.entities.append(entity)
-         return self
+pygame.init()
+screen_width = 700
+screen_height = 700
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Bloockey')
+tile_size = 50
+bg_img = pygame.image.load('img/sky.png')
 
-     def addUserInterface(self, userInterface):
-         self.userInterfaces.append(userInterface)
-         return self
-
-     def addBlock(self, block):
-         self.blocks.append(block)
-         return self
-
-     def addFront(self, front):
-         self.fronts.append(front)
-         return self
-
-class Game():
-    def __int__(self):
-        self.scene = ""
-        self.running = False
-        self.screen = ""
-
-    def setUp(self):
-        (numpass, numfail) = pygame.init()
-
-        print('Pygames Modules:\n',
-              numpass, ' sucess\n',
-              numfail, ' fails\n'
-              )
-
-        print('PygameInit :', pygame.get_init(), '\n')
-
-        pygame.display.set_icon(ICON)
-
-        pygame.display.set_caption(TITLE)
-
-        self.screen = pygame.display.set_mode((700, 700))
-
-        x, y = self.screen.get_size()
-        print("Screen : {:^4} x {:^4}\n".format(x, y))
-
-        self.running = True
-
-    def run(self):
-
-        while self.running:
-            pygame.display.flip()
-
-            self.eventsHandler()
-
-    def eventsHandler(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.running = False
-                pygame.quit()
-                #ferme la fenetre
-                exit()
-
-
-class Blocks(pygame.sprite.Sprite, Game):
-    def __init__(self, img, x = 350, y = 350):
-        super().__init__()
-        self.x = x
-        self.y = y
-        self.img = pygame.image.load("graphics/",img,".png").convert_alpha()
-        self.rect = self.img.get_rect()
-
-    def blit(self):
-        self.screen.blit(self.img, self.rect)
-
-
-class Font(Game):
-    def __init__(self, text, x = 350, y = 350, color = (111, 196, 169), backGround = None):
-        self.x = x
-        self.y = y
-        self.text= text
-        self.color = color
-        self.backGround = backGround
-
-    def blit(self):
-        game_message = getFont().render(self.text, False, self.color, self.backGround)
-        game_message_rect = game_message.get_rect(center=(self.x, self.y))
-        self.screen.blit(game_message, game_message_rect)
-
-def getFont(tall = 50):
-    return pygame.font.Font('font/Pixeltype.ttf', tall)
-
-GAME = 'the game object'
-if __name__ == "__main__":
-    TITLE = "Block"
-    ICON = pygame.image.load("graphics/logo32x32.jpg")
-    RED = (100, 0, 0)
-
-    GAME = Game()
-    GAME.setUp()
-    GAME.run()
-
-level_map = [
-[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1],
-[1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1],
-[1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1],
-[1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1],
-[1, 0, 0, 0, 0, 0, 2, 2, 2, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1],
-[1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-[1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-[1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+world_data = [
+[1, 1, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0],
+[1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+[1, 0, 2, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+[1, 0, 0, 2, 0, 0, 2, 0, 0, 0, 0, 2, 0, 0],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0],
+[1, 0, 0, 0, 0, 0, 2, 2, 2, 1, 1, 1, 1, 1],
+[1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
-level.World(level_map)
+world = level.World(world_data)
+run = True
+while run:
+    screen.blit(bg_img, (0, 0))
+    world.draw()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    pygame.display.update()
+pygame.quit()
