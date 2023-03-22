579e8dd2b472d8ae2dbf4db8699b8e3e22ac372d
AstrantV <AstrantV.creator@gmail.com>
2023-03-22 13:40:39 +0100

    Update KeyDown.py
    a

--- events/KeyDown.py
+++ events/KeyDown.py
@@ -5,5 +5,5 @@ class Exe:
     def exe(self, game, event):
         module = importName("events." + pygame.key.name(event.key), "Exe") #classe
         module = module() #nouvelle instance de la classe
-        
+
         module.exe(game, event) #exécution d'une fonction dans l'instance créée
