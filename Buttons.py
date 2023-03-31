fd974982579206da2fd9bb2786615024d405148d
AstrantV <AstrantV.creator@gmail.com>
2023-03-31 14:57:44 +0200

    a

--- Buttons.py
+++ Buttons.py
@@ -2,10 +2,18 @@ from abc import abstractmethod
 import Bricks
 
 
-class Button(Bricks.Brick):
+class Button:
     def __init__(self):
-        super().__init__()
-
-    @abstractmethod
-    def exe(self):
-        return 1
+        font = None
+        text = ''
+        textSize = 1
+        color = "white"
+        x = 0
+        y = 0
+        rect = None
+        id = None
+²   axxc 
+    def blit(self, screen):
+        font = pygame.font.SysFont(self.font, self.size)
+        img = police_pause.render(self.text, 1, self.color)
+        self.screen.blit(song_pause, (self.x, self.y))
